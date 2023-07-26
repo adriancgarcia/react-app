ee17ec183992f4bae9d3ae3e73f15c7d455e16a9
adriancgarcia <adriancgarcia@gmail.com>
2023-07-25 20:14:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const GrandChildOne = (props) => {
+  return (
+    <>
+    <h3>{props.bookTitle}</h3>
+      <h3>{props.author}</h3>
+    </>
+  )
+}
+
+
+const ChildOne = (props) => {
+  return (
+    <>
+    <h1>{props.bookTitle}</h1>
+    {props.author && <GrandChildOne bookTitle={props.bookTitle} author={props.author}/>}
+      </>
+    )
+}
+
+const ChildOneVersionTwo = (props) => {
+  return (
+    <>
+      <h1>{props.bookTitle}</h1>
+      <GrandChildOne
+        bookTitle={props.bookTitle}
+        author={props.author}
+      />
+    </>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <ChildOne bookTitle = "100 years of Solitude" />
+      <ChildOne bookTitle ="Love in the Time of Cholera"/>
+      <ChildOneVersionTwo author = "Rodolfo Anaya" bookTitle = "Bless me, Ultima" />
     </div>
   );
-}
+} 
 
 export default App;
